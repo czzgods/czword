561c06a9d56fb11bec7c4acbaaee8ac02a1fc957
cz <3302076969@qq.com>
2024-04-05 20:36:29 +0800

    使用AOP技术，将RabbitMQ的配置信息通过注解的方式导入业务模块中

---
+++ czword-common/common-service/src/main/java/com/itcz/czword/common/service/annotation/EnableOrderDelayRabbitmq.java
@@ -0,0 +1,15 @@
+package com.itcz.czword.common.service.annotation;
+
+import com.itcz.czword.common.service.config.OrderRabbitMqConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(OrderRabbitMqConfig.class)
+public @interface EnableOrderDelayRabbitmq {
+}
---
+++ czword-common/common-service/src/main/java/com/itcz/czword/common/service/annotation/unify/OrderServiceConfig.java
@@ -0,0 +1,23 @@
+package com.itcz.czword.common.service.annotation.unify;
+
+import com.itcz.czword.common.service.annotation.*;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@EnableUserAspect //允许进行权限校验
+@EnableWebConfig //登录拦截器
+@EnableRedisConfig //redis配置
+@EnableSwRateLimiting //开启滑动限流
+//@EnableLog //开启日志
+@EnablePage //开启分页
+@EnableRedisLock //开启分布式锁
+@EnableFeignInterceptor //开启Feign拦截器
+@EnableAysnThreadPool //开启异步线程池
+@EnableOrderDelayRabbitmq //开启RabbitMQ
+public @interface OrderServiceConfig {
+}
